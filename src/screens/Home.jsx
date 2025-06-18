c3856b8fd06f652ff58dad4843380122ce6ff634
Md Naseer uddin <umdnaseer17@gmail.com>
2025-06-18 17:47:03 +0530

    improved pdf nav

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -17,21 +17,48 @@ const dummyPDFs = [
     id: '1',
     title: 'Sample PDF',
     size: '2.5 MB',
-    pages: 10,
-    uri: 'bundle-assets://sample.pdf',
+    pages: 2,
+    uri: "https://www.adobe.com/support/products/enterprise/knowledgecenter/media/c4611_sample_explain.pdf"
   },
   {
     id: '2',
-    title: 'Sample PDF',
-    size: '2.5 MB',
-    pages: 10,
-    uri: 'bundle-assets://sample.pdf',
+    title: 'Dummy PDF',
+    size: '1.5 MB',
+    pages: 1,
+    uri: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+  },
+  {
+    id: '3',
+    title: 'Invoice Sample',
+    size: '22.5 MB',
+    pages: 1,
+    uri: "https://www.princexml.com/samples/invoice-colorful/invoicesample.pdf"
+  },
+  {
+    id: '4',
+    title: 'Example PDF',
+    size: '32.5 MB',
+    pages: 3,
+    uri: "https://www.princexml.com/samples/usenix/example.pdf"
+  },{
+    id: '5',
+    title: 'Essay',
+    size: '55 MB',
+    pages: 408,
+    uri: "https://www.princexml.com/samples/essay.pdf"
+  },
+  {
+    id: '6',
+    title: 'Magic Book',
+    size: '2.56 MB',
+    pages: 2,
+    uri: "https://www.princexml.com/samples/magic6/magic.pdf"
   },
 ];
 
 export default function Home({navigation}) {
   const openPDF = item => {
-    navigation.navigate('ViewScreen', {pdf: item});
+    navigation.navigate('ViewScreen', {url: item.uri});
   };
 
   console.log('Started2');
@@ -41,24 +68,18 @@ export default function Home({navigation}) {
   return (
     <View style={styles.container}>
       <Text style={styles.header}>PDF Viewer</Text>
-      Alert.alert('Welcome to PDF Viewer');
+      {/* Alert.alert('Welcome to PDF Viewer'); */}
 
       <View style={{flex: 1, paddingHorizontal: 10}}>
         <FlatList
-          data={[
-            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-            1, 1, 1,
-          ]}
+          data={dummyPDFs}
           horizontal={false}
           showsHorizontalScrollIndicator={false}
           numColumns={2}
-          keyExtractor={(item, index) => index.toString()}
-          // keyExtractor={(item) => item.id}
+          // keyExtractor={(item, index) => index.toString()}
+          keyExtractor={(item) => item.id}
           renderItem={({item, index}) => (
             <TouchableOpacity style={styles.card} onPress={() => openPDF(item)}>
-              {/* <Text style={styles.title}>{item.title}</Text>
-            <Text>Size: {item.size}</Text>
-            <Text>Pages: {item.pages}</Text> */}
               <Image
                 source={require('../assets/pdf.webp')}
                 style={{
@@ -68,7 +89,12 @@ export default function Home({navigation}) {
                   resizeMode: 'contain',
                 }}
               />
-              <Text style={styles.title}>Book {index + 1}</Text>
+              <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Text style={styles.title}>Size: {item.size}</Text>
+              <Text style={styles.title}>Pages: {item.pages}</Text>
+              </View>
+              {/* <Text style={styles.title}>Book {index + 1}</Text> */}
             </TouchableOpacity>
           )}
         />
@@ -101,7 +127,7 @@ const styles = StyleSheet.create({
   title: {
     fontWeight: 'bold',
     alignSelf: 'center',
-    margin: 18,
-    fontSize: 26,
+    margin: 1,
+    fontSize: 20,
   },
 });
--- src/screens/ViewScreen.jsx
+++ src/screens/ViewScreen.jsx
@@ -1,8 +1,12 @@
 import {ActivityIndicator, Dimensions, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import Pdf from 'react-native-pdf';
+import { useRoute } from '@react-navigation/native';
 
 const ViewScreen = () => {
+  const route = useRoute();
+  const { url } = route.params;
+
   return (
     <View style={styles.container}>
                 <Pdf
@@ -10,7 +14,7 @@ const ViewScreen = () => {
                     horizontal
                     enablePaging={true}
                     onLoadProgress={()=><ActivityIndicator size="large" color="red" />}
-                    source={{uri: "https://www.adobe.com/support/products/enterprise/knowledgecenter/media/c4611_sample_explain.pdf"}}
+                    source={{ uri: url }}
                     // source={{uri: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"}}
                     onLoadComplete={(numberOfPages,filePath) => {
                         console.log(`Number of pages: ${numberOfPages}`);
